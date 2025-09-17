388513bc2e470dcccedda3df763a9d28e1424c8e
Your Name <you@example.com>
2025-09-17 10:06:20 +0500

    render connected

--- src/components/TextGenerator.jsx
+++ src/components/TextGenerator.jsx
@@ -13,7 +13,7 @@ export default function TextGenerator() {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/generate", {
+      const res = await axios.post("https://bc-1kwq.onrender.com/api/generate", {
         prompt,
       });
 
